b7f06770daeff9a5ffd98d79123ce86588ed7627
fadal.error <fadal.error@gmail.com>
2021-06-18 10:46:02 +0200

    applied different styles to buttons

--- src/App/Forms/Buttons/index.js
+++ src/App/Forms/Buttons/index.js
@@ -2,10 +2,10 @@ import "./style.css";
 
 const Buttons = () => (
     <div className="form__buttonsContainer">
-        <button className="buttons__button" type={"submit"}>
+        <button className="buttons__button buttons__button--submit" type={"submit"}>
             Oblicz
         </button>
-        <button className="buttons__button" type="reset">
+        <button className="buttons__button buttons__button--reset" type="reset">
             Wyczyść
         </button>
     </div>
