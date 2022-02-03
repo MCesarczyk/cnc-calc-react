091c88f44710bc851bb3060bd5eee2d085e5ebf2
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-02-03 13:46:31 +0100

    create Form as wrapper component

--- src/App/Forms/Form1/index.js
+++ src/App/Forms/Form1/index.js
@@ -1,12 +1,7 @@
 import { useRef, useState } from "react";
 import languages from "../../../assets/fixtures/languages";
+import Form from "../../../components/Form";
 import FormButtons from "../../../components/FormButtons";
-import {
-  Form,
-  Fieldset,
-  Legend,
-  FieldsContainer
-} from "../styled.js";
 import FormField from "../../../components/FormField";
 
 const Form1 = ({ legend, langId }) => {
@@ -30,46 +25,45 @@ const Form1 = ({ legend, langId }) => {
   };
 
   return (
-    <Form onSubmit={onFormSubmit} onReset={onFormReset} >
-      <Fieldset>
-        <Legend>{legend}</Legend>
-        <FieldsContainer>
-          <FormField
-            name={languages[langId].diameter.name}
-            unit={languages[langId].diameter.unit}
-            inputRef={inputRef}
-            value={diameter}
-            type="number"
-            min="0.0001"
-            step="0.0001"
-            placeholder={languages[langId].diameter.placeholder}
-            required
-            autoFocus
-            onChange={({ target }) => setDiameter(target.value - 0)}
-          />
-          <FormField
-            name={languages[langId].rotSpeed.name}
-            sub={languages[langId].rotSpeed.sub}
-            unit={languages[langId].rotSpeed.unit}
-            value={rotationSpeed}
-            type="number"
-            min="1"
-            step="1"
-            placeholder={languages[langId].rotSpeed.placeholder}
-            required
-            onChange={({ target }) => setRotationSpeed(target.value)}
-          />
-          <FormField
-            name={languages[langId].cutSpeed.name}
-            sub={languages[langId].cutSpeed.sub}
-            unit={languages[langId].cutSpeed.unit}
-            value={cuttingSpeed}
-            readOnly
-            placeholder={languages[langId].cutSpeed.placeholder}
-          />
-        </FieldsContainer>
-        <FormButtons langID={langId} />
-      </Fieldset>
+    <Form
+      legend={legend}
+      onSubmit={onFormSubmit}
+      onReset={onFormReset}
+      footerContent={<FormButtons langID={langId} />}
+    >
+      <FormField
+        name={languages[langId].diameter.name}
+        unit={languages[langId].diameter.unit}
+        inputRef={inputRef}
+        value={diameter}
+        type="number"
+        min="0.0001"
+        step="0.0001"
+        placeholder={languages[langId].diameter.placeholder}
+        required
+        autoFocus
+        onChange={({ target }) => setDiameter(target.value - 0)}
+      />
+      <FormField
+        name={languages[langId].rotSpeed.name}
+        sub={languages[langId].rotSpeed.sub}
+        unit={languages[langId].rotSpeed.unit}
+        value={rotationSpeed}
+        type="number"
+        min="1"
+        step="1"
+        placeholder={languages[langId].rotSpeed.placeholder}
+        required
+        onChange={({ target }) => setRotationSpeed(target.value)}
+      />
+      <FormField
+        name={languages[langId].cutSpeed.name}
+        sub={languages[langId].cutSpeed.sub}
+        unit={languages[langId].cutSpeed.unit}
+        value={cuttingSpeed}
+        readOnly
+        placeholder={languages[langId].cutSpeed.placeholder}
+      />
     </Form >
   )
 };
