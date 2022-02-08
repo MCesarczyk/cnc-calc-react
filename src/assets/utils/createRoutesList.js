8410915d6ecb99436e25ae7e733a79a0d9b5d69e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-02-08 09:23:46 +0100

    create routes rendering function

---
+++ src/assets/utils/createRoutesList.js
@@ -0,0 +1,10 @@
+import languages from "../fixtures/languages";
+
+export const createRoutesList = (langId) => {
+  return [
+    { id: 1, path: '/surface-speed', label: languages[langId]?.form1Legend },
+    { id: 2, path: '/spindle-speed', label: languages[langId]?.form2Legend },
+    { id: 3, path: '/feedrate', label: languages[langId]?.form3Legend },
+    { id: 4, path: '/tapping-feed', label: languages[langId]?.form4Legend },
+  ];
+};
--- src/features/Navigation/index.js
+++ src/features/Navigation/index.js
@@ -1,14 +1,9 @@
-import languages from "../../assets/fixtures/languages";
+import { createRoutesList } from "../../assets/utils/createRoutesList";
 import { Item, List } from "./styled";
 
-
 const Navigation = ({ langId }) => {
-  const routes = [
-    { id: 1, path: '/surface-speed', label: languages[langId].form1Legend },
-    { id: 2, path: '/spindle-speed', label: languages[langId].form2Legend },
-    { id: 3, path: '/feedrate', label: languages[langId].form3Legend },
-    { id: 4, path: '/tapping-feed', label: languages[langId].form4Legend },
-  ];
+
+  const routes = createRoutesList(langId);
 
   return (
     <nav>
