27af70afe97cfefe5c1a0a1fba9d87a9deae5754
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-02-15 08:33:17 +0100

    fix type error when passing prop to Navigation

--- src/features/Sidebar/index.js
+++ src/features/Sidebar/index.js
@@ -6,32 +6,33 @@ const Sidebar = ({ langId, open, setOpen }) => {
   const { pathname } = useLocation();
 
   const handleOpen = () => {
-      setTimeout(() => {
-          setOpen(true);
-      }, 1_000);
+    setTimeout(() => {
+      setOpen(true);
+    }, 1_000);
   };
 
   const handleClose = () => {
     setOpen(false);
   };
 
-  return(
-  <>
-    <SidebarActivationArea
-      onPointerEnter={handleOpen}
-    />
-    <SidebarNavigation
-      visible={open && pathname !== "/"}
-      onPointerLeave={handleClose}
-    >
-      <Navigation
-        langId={langId}
-        sidebar
-        baseUrl="cnc-calc-react/"
-        handleClose={handleClose}
+  return (
+    <>
+      <SidebarActivationArea
+        onPointerEnter={handleOpen}
       />
-    </SidebarNavigation>
-  </>
-)};
+      <SidebarNavigation
+        visible={open && pathname !== "/"}
+        onPointerLeave={handleClose}
+      >
+        <Navigation
+          langId={langId}
+          sidebar="true"
+          baseUrl="cnc-calc-react/"
+          handleClose={handleClose}
+        />
+      </SidebarNavigation>
+    </>
+  )
+};
 
 export default Sidebar;
